ce2cd3af0b15148b4a80666938d57b1340263170
duy-tu2005 <quachduytu22@gmail.com>
2026-03-14 13:57:12 +0700

    Add files via upload

--- feature/booking/views.py
+++ feature/booking/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from .models import Booking
 
@@ -45,3 +46,31 @@ def create_booking(request):
         return redirect("booking_page")
 
     return redirect("booking_page")
+=======
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Booking
+from .serializers import BookingSerializer
+
+
+@api_view(["GET", "POST"])
+def booking_list_create(request):
+    if request.method == "GET":
+        bookings = Booking.objects.all().order_by("-id")
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+
+    serializer = BookingSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def booking_test(request):
+    return Response({"message": "Booking API is working"})
+>>>>>>> fdbb98c (Add files via upload)
